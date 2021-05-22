fef5a6ad78f3c3bfc289a082a68991ea16200c71
maaqoul <hicham.maaqoul@gmail.com>
2021-05-22 22:41:01 +0100

    add router

--- src/app.js
+++ src/app.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Details from './Details';
 import SearchParams from './SearchParams'
 
 const App = () => {
     return (
         <div>
             <h1>Adopt Me!</h1>
-            <SearchParams />
+            <Router>
+                <Switch>
+                    <Route path="/details/:id">
+                        <Details />
+                    </Route>
+                    <Route path="/">
+                        <SearchParams />
+                    </Route>
+                </Switch>
+            </Router>
         </div>
     )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+    document.getElementById("root"));
